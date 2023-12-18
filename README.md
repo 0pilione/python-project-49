de2cc71227194a34a2793510b07dbd058ecf5759
Татьяна <deepredoctober@gmail.com>
2023-12-18 15:41:23 +0300

    experiment1

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 [![Actions Status](https://github.com/0pilione/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/0pilione/python-project-49/actions)
 
 <a href="https://codeclimate.com/github/0pilione/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/129c5a7c7d7257747d0d/maintainability" /></a>
+
+brain-even: https://asciinema.org/connect/f44c5abe-f1db-4cfd-99f5-eb29c912ddde
+brain-calc: https://asciinema.org/a/qk83dY7K0oxgrRUciKhPRcwmQ
+brain-gcd: https://asciinema.org/a/cUbWJP8r5neEeAm8LjRppehBm
+brain-progression: https://asciinema.org/a/IJCiIa2gc18lS1at8WXTP9VfX
+brain-prime: https://asciinema.org/a/VXLcXKyd2hv5DQJfPJU7b5LBS
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,12 @@
 from random import randint
 
-def start_game() -> Literal[1, 0]:
-    random_number: int = randint(a=1, b=100)
+def main():
+    ab = 'x'
+
+def engine():
+    random_number = randint(1, 100)
     print(f"Question: {random_number}")
-    user_answer: str = input('Your answer: ')
+    user_answer = input('Your answer: ')
     if (user_answer == 'yes' and random_number % 2 == 0) or (user_answer == 'no' and random_number % 2 != 0):
         print('Correct!')
         return 1
@@ -17,20 +20,18 @@ def start_game() -> Literal[1, 0]:
         print(f"it is wrong answer ;(.\n Let's try again, {name}!")
         return 0
 
-def main() -> None:
-    name = input('''Welcome to the Brain Games!
+name = input('''Welcome to the Brain Games!
 May I have your name? ''' )
-    print(f"Hello, {name}!\nAnswer 'yes' if the number is even, otherwise answer 'no'.")
-    win = 3
-    index = 0
-    result = 0
+print(f"Hello, {name}!\nAnswer 'yes' if the number is even, otherwise answer 'no'.")
+win = 3
+index = 0
+result = 0
 
-    while index < win:
-        result: Literal[1, 0] = start_game()
-        if result == 0:
-            break
-        index += 1
-
-
-    if result == 1:
-        print(f'Congratulations, {name}!')
+while index < win:
+    result = engine()
+    if result == 0:
+        break
+    index += 1
+    
+if result == 1:
+    print(f'Congratulations, {name}!')
