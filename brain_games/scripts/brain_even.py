a7c65fac5595a3038e6b47fdb66c132983d78280
Татьяна <deepredoctober@gmail.com>
2024-01-15 17:42:07 +0300

    calc+even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,28 +1,35 @@
 from random import randint
 
+
 def main():
     ab = 'x'
+
 
 def engine():
     random_number = randint(1, 100)
     print(f"Question: {random_number}")
     user_answer = input('Your answer: ')
-    if (user_answer == 'yes' and random_number % 2 == 0) or (user_answer == 'no' and random_number % 2 != 0):
+    if (user_answer == 'yes' and random_number % 2 == 0) 
+ or (user_answer == 'no' and random_number % 2 != 0):
         print('Correct!')
         return 1
-    elif user_answer == 'yes' and random_number % 2 !=0:
-        print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, {name}!")
+    elif user_answer == 'yes' and random_number % 2 != 0:
+        print(f"""yes' is wrong answer ;(.
+Correct answer was 'no'.\n Let's try again, {name}!""")
         return 0
     elif user_answer == 'no' and random_number % 2 == 0:
-        print(f"'no' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, {name}!")
+        print(f"""'no' is wrong answer ;(.
+Correct answer was 'yes'.\n Let's try again, {name}!""")
         return 0
     else:
         print(f"it is wrong answer ;(.\n Let's try again, {name}!")
         return 0
 
+
 name = input('''Welcome to the Brain Games!
-May I have your name? ''' )
-print(f'Hello, {name}!\nAnswer "yes" if the number is even, otherwise answer "no".')
+May I have your name? ''')
+print(f'''Hello, {name}!
+Answer "yes" if the number is even, otherwise answer "no".''')
 win = 3
 index = 0
 result = 0
@@ -32,6 +39,6 @@ while index < win:
     if result == 0:
         break
     index += 1
-    
+
 if result == 1:
     print(f'Congratulations, {name}!')
