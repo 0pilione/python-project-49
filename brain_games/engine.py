2bd6a4bad8a20b05ce849c5e6388b1cf39186d6d
Татьяна <deepredoctober@gmail.com>
2024-02-11 19:40:24 +0300

    fd

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -11,14 +11,14 @@ def engine(question, instructions):
     while count < 3:
         task, res = question()
         print(f"Question: {task}")
-        user_answ = input('Your answer: ')
-        if user_answ == res:
+        usr_answ = input('Your answer: ')
+        if usr_answ == res:
             print('Correct!')
             check_res = 1
             count += 1
             continue
         else:
-            print(f'{user_answ} is wrong answer ;(.\n Correct answer was {res}.')
+            print(f'{usr_answ} is wrong answer ;(.\n Correct answer was {res}.')
             check_res = 0
             break
     if check_res == 1:
